a77208acccab1b550b23fc8786f2c77f01ea04ba
nsx_69 <mrmanafest16@gmail.com>
2024-01-07 13:53:57 +0500

    tasks with string

---
+++ script.js
@@ -0,0 +1,22 @@
+// let str = 'abc';
+// alert(str);
+
+// let str = '!!!';
+// alert(str);
+
+// let a = 'Java';
+// let b = 'Script';
+// let str = a +b;
+// alert(str);
+
+// let str = 'abcde';
+// alert(str.length);
+
+// let str = 'ab c de';
+// alert(str.length);
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = 'aaa ' + str1 + ' bbb ' + str2 + ' ccc';
+// alert(txt)
+// alert(txt.length)
